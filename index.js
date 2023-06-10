29e6ece84ab2db88b415055d1ef06ecc11fd6d29
Maxim <334890@gmail.com>
2023-06-10 11:55:55 +0600

    include forgotten index.js and packaje.json files

---
+++ index.js
@@ -0,0 +1,7 @@
+import LinkedList from "./LinkedList.js";
+
+const newList = LinkedList();
+console.log(newList);
+console.log(newList.append(2));
+console.log(newList.prepend(1));
+console.log(newList);
